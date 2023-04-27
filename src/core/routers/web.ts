0383cf91fd49cf10ca95d8e1a4d15802e24ce601
milyasbpa <basilyas.2015@gmail.com>
2023-04-28 00:14:38 +0700

    fix: developer

--- src/core/routers/web.ts
+++ src/core/routers/web.ts
@@ -2,3 +2,5 @@ export const routeToHome = () => "/";
 export const routeToDevelopers = () => "/developer";
 export const routeToGenres = () => "/genre";
 export const routeToGame = (id: number) => `/game/${id}`;
+export const routeToGenre = (name: string) => `/genre/${name}`;
+export const routeToDeveloper = (name: string) => `/developer/${name}`;
